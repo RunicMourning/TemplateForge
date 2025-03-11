26e693594b375bab215cac3fb560e56faded962c
RunicMourning <templatesforall@gmail.com>
2025-03-10 23:23:36 -0500

    CSS Shenanigans

--- templates/main_template.php
+++ templates/main_template.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo htmlspecialchars($pageTitle); ?> - <?php echo htmlspecialchars($siteTitle); ?></title>
+    <link rel="alternate" type="application/rss+xml" href="/rss.php" title="<?php echo htmlspecialchars($siteTitle); ?> RSS Feed" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cairo+Play:wght@200..1000&family=Nabla&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"><link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 
 <!-- Output header includes -->
 <?php
@@ -13,14 +16,12 @@ if (!empty($headerIncludes)) {
     echo implode("\n", $headerIncludes);
 }
 ?>
-<link rel="alternate" type="application/rss+xml" href="/rss.php" title="<?php echo htmlspecialchars($siteTitle); ?> RSS Feed" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container-fluid mt-4">
     <!-- Header -->
     <header class="bg-light py-3 text-start p-2">
-        <h1><?php echo htmlspecialchars($siteTitle); ?></h1>
+        <h1><span class="roboto">Template</span><b>Forge</b></h1>
     </header>
     
     <!-- Navigation Bar -->
@@ -45,12 +46,12 @@ if (!empty($headerIncludes)) {
     <!-- Main Layout -->
         <div class="row">
             <!-- Sidebar -->
-            <aside class="col-md-3">
+            <aside class="col-md-3 order-1 order-md-0">
 <?php loadSidebars(); ?>
             </aside>
             
             <!-- Main Content -->
-            <main class="col-md-9">
+            <main class="col-md-9 order-0 order-md-1">
 <?php echo $pageContent; ?>
             </main>
         </div>
